6ad0238b7e394d8cfc40c891c91b90588d9d6616
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-07-26 19:33:55 -0500

    Java

---
+++ Java/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args)
+     {
+        System.out.println("Hola Mundo");
+        Car car = new Car("AMQ123", new Account ("Andres Herrera", "AND123"));
+        car.passegenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car("YZL367", new Account("Andrea Herrera", "ANDA876"));
+        car2.passegenger= 3;
+        car2.printDataCar();
+        
+
+    } 
+
+    }
