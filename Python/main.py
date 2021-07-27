905cbcb565e0bf2cf331e0343640c23b69dbe91c
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-07-26 20:09:10 -0500

    Python

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+form car import car
+
+if__name__ == "__main__":
+    print("Hola Mundo")
+    car = Car()
+    car.license = "ZBS999"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE816"
+    car.2driver = "Martha Jimenez"
+    print(vars(car2))
+    
+
+    
