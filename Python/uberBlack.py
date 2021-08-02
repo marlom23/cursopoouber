00212559e5859719a08d28b60f520e2d53405f63
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-08-02 16:16:36 -0500

    Se crea de Herencia de Car  Python

---
+++ Python/uberBlack.py
@@ -0,0 +1,11 @@
+from _typeshed import Self
+from car import Car
+
+class UberBlack(Car):
+    typeCarAccepted = []
+    seatsMaterial = []
+
+    def __init__(license, driver)(self, license, driver, typeCarAccepted, seatsmaterial):
+        super().__init__(license, driver)
+        self.typeCarAccepted = typeCarAccepted
+        Self.seatsMateria; = seatsMaterial
---
+++ Python/uberVan.py
@@ -0,0 +1,10 @@
+from car import Car
+
+class UberVan(Car):
+    typeCarAccepted = []
+    seatsMaterial = []
+
+    def __init__(license, driver)(self, license, driver, typeCarAccepted, seatsmaterial):
+        super().__init__(license, driver)
+        self.typeCarAccepted = typeCarAccepted
+        Self.seatsMateria; = seatsMaterial
