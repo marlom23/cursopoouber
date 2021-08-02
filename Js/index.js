437d7f594f0e024bbef24c5c9d7041bd847d42e3
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-08-02 17:25:08 -0500

    Se crea Herencia de Car js

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,9 @@
-var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
+var car = new Car("ZBS999", new Account("Andres Herrera", "AND876"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("ZBS999", new Account("Andres Herrera", "AND876"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
+console.log("uberX");
+
